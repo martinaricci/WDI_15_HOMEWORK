97a4df40eb7f274463480f267a15668d8a8c93a3
Martina Ricci <martinaricci@Martinas-MacBook-Air.local>
2018-03-15 16:58:33 +1100

    array_homework_completed

---
+++ Martina/wk01/4-thu/array.js
@@ -0,0 +1,19 @@
+console.log('hello arrays')
+
+var dayOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+
+
+dayOfTheWeek.pop();
+
+var newLength = dayOfTheWeek.unshift('sunday');
+
+var dayOfTheWeek = [['monday', 'tuesday', 'wednesday', 'thursday', 'friday'], ['saturday', 'sunday']];
+
+//Remove the weekdays
+var newLength = dayOfTheWeek.shift();
+
+//Remove the weeekends
+//dayOfTheWeek.pop([]);
+
+var dayOfTheWeek = ['saturday', 'sunday'];
