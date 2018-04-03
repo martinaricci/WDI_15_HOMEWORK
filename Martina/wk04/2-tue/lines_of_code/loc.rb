c43d9d7fb98ae013df1dd8f2fe45477fb57f41e9
Martina Ricci <martinaricci@Martinas-MacBook-Air.local>
2018-04-03 21:59:04 +1000

    tried to finish my homework

---
+++ Martina/wk04/2-tue/lines_of_code/loc.rb
@@ -0,0 +1,16 @@
+# write a program loc.rb to count and puts the number of lines of code in a file
+
+# eg. say you have a file data.txt with the following contents
+
+# cat
+# dog
+# rabbit
+# when I run the program, it should prompt for a filename
+
+# $ ruby loc.rb
+# $ Enter filename: data.txt
+# $ 3 line(s)
+
+gets.chomp.to_s
+readlines
+
